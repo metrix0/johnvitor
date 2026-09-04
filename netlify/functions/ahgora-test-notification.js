de37f71d9d8f792a0e2910b2da246ef2aedbc965
João Vitor Redondano de Almeida <j@johnvitor.com>
2026-09-04 15:44:42 -0300

    Add ALARM TRIGGER to ntfy test title

--- netlify/functions/ahgora-test-notification.js
+++ netlify/functions/ahgora-test-notification.js
@@ -29,7 +29,7 @@ exports.handler = async function(event) {
       method: 'POST',
       headers: {
         'Content-Type': 'text/plain; charset=utf-8',
-        'Title': 'Ahgora: teste de falha',
+        'Title': 'ALARM TRIGGER - Ahgora: teste de falha',
         'Priority': '5',
         'Tags': 'warning,rotating_light',
         'Click': `${SITE_URL}/ahgora`
